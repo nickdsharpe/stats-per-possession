fce075abb90db74c7d74c9dc0776a1bfe5dd3d19
Nick Sharpe <nickdsharpe@comcast.net>
2023-09-15 13:49:58 -0600

    Progress on Shot Locations

--- src/components/court.tsx
+++ src/components/court.tsx
@@ -1,6 +1,6 @@
 import Plot from "react-plotly.js";
-import initial_state from "../assets/initial_state.json";
 import courtImage from "../assets/court_image.jpg";
+import example_data from "../assets/example_data.json";
 
 function DrawPlotlyCourt() {
   const layout: Partial<Plotly.Layout> = {
@@ -17,39 +17,60 @@ function DrawPlotlyCourt() {
         layer: "below",
         xanchor: "center",
         yanchor: "bottom",
+        sizing: "stretch",
       },
     ],
     xaxis: {
       range: [-250, 250],
       showgrid: false,
       showticklabels: false,
+      visible: false,
     },
     yaxis: {
       range: [-52.5, 417.5],
       showgrid: false,
       showticklabels: false,
+      visible: false,
     },
     showlegend: false,
+    hovermode: false,
+    dragmode: false,
   };
 
-  // Define your scatter trace data
-  const scatterTrace: Partial<Plotly.PlotData> = {
-    type: "scatter",
-    x: [-250, 0],
-    y: [-52.5, 237.5],
-    mode: "markers",
-    marker: {
-      opacity: 1.0,
-      size: 5,
-    },
-  };
+  // Create Marker data for selected shot locations
+  const scatterData: any = [];
+
+  example_data["shooting_locations"].forEach((location) => {
+    const coordinates = location[0] as [number, number];
+    const isGreenMarker = location[1] === 1;
+
+    const markerColor = isGreenMarker ? "#45e64c" : "f05443";
+
+    const scatterTrace = {
+      type: "scatter",
+      mode: "markers",
+      x: [coordinates[0]],
+      y: [coordinates[1]],
+      marker: {
+        color: "rgba(0, 0, 0, 0)",
+        size: 24,
+        symbol: "circle",
+        line: {
+          color: markerColor,
+          width: 6,
+        },
+      },
+    };
+
+    scatterData.push(scatterTrace);
+  });
 
   return (
     <Plot
-      data={[scatterTrace]}
+      data={scatterData}
       layout={layout}
-      config={{ displayModeBar: false }}
-      style={{ width: 950, height: 940 }}
+      config={{ displayModeBar: false, scrollZoom: false }}
+      style={{ width: 1000, height: 940 }}
     />
   );
 }
