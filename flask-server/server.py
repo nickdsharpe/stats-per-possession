9294353739c6ddd84ec1f17ba5cb018f96b38e32
Nick Sharpe <nickdsharpe@comcast.net>
2023-10-19 09:32:25 -0600

    Working Shot Locations

--- flask-server/server.py
+++ flask-server/server.py
@@ -9,15 +9,24 @@ CORS(app)
 @app.route('/process_json', methods=['POST'])
 def process_json():
     try:
+        return_data = {}
         # Get the JSON data from the request
         data = request.get_json()
+        ppp_data = data['data']
+        shooting_locations = data['shooting_locations']
+        created_locations = data['created_locations']
         
         # Process the data
-        df = pd.DataFrame(data).transpose()
+        df = pd.DataFrame(ppp_data).transpose()
         processed_data = PPP(df)
-
+        
+        # Make return Dictionary
+        return_data['data'] = processed_data.reset_index().rename(columns={'index': 'Shot Type'}).to_dict(orient='records')
+        return_data['shooting_locations'] = shooting_locations
+        return_data['created_locations'] = created_locations
+    
         # Return the updated JSON data as a response
-        return processed_data.reset_index().rename(columns={'index': 'Shot Type'}).to_dict(orient='records')
+        return return_data
 
     except Exception as e:
         # Handle any exceptions that may occur during processing
